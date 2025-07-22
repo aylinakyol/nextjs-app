ed6bfba38c21433f45f5a034d3d3ce6cdfd11dfd
aylinakyol <akyolaylin2@gmail.com>
2025-07-22 16:29:47 +0300

    url düzeltmesi

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 const PAYLOAD_API_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000/api'
-    : `${process.env.NEXT_PUBLIC_SITE_URL}/api`;
+    : `${process.env.NEXT_PUBLIC_API_URL}`;
 
 
 export async function getLatestPosts() {
